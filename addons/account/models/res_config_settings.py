61e5bc9bfc691a9f6b33750b592c704267d26659
Md. Shaheen Hossain <rapidgrps@princegroup-bd.com>
2021-03-21 02:36:22 +0600

    remove all enterprise from accounting

--- addons/account/models/res_config_settings.py
+++ addons/account/models/res_config_settings.py
@@ -55,8 +55,6 @@ class ResConfigSettings(models.TransientModel):
         "Show line subtotals with taxes (B2C)",
         implied_group='account.group_show_line_subtotals_tax_included',
         group='base.group_portal,base.group_user,base.group_public')
-    group_show_sale_receipts = fields.Boolean(string='Sale Receipt',
-        implied_group='account.group_sale_receipts')
     group_show_purchase_receipts = fields.Boolean(string='Purchase Receipt',
         implied_group='account.group_purchase_receipts')
     show_line_subtotals_tax_selection = fields.Selection([
@@ -64,28 +62,12 @@ class ResConfigSettings(models.TransientModel):
         ('tax_included', 'Tax-Included')], string="Line Subtotals Tax Display",
         required=True, default='tax_excluded',
         config_parameter='account.show_line_subtotals_tax_selection')
-    module_account_budget = fields.Boolean(string='Budget Management')
     module_account_payment = fields.Boolean(string='Invoice Online Payment')
     module_account_reports = fields.Boolean("Dynamic Reports")
     module_account_check_printing = fields.Boolean("Allow check printing and deposits")
-    module_account_batch_payment = fields.Boolean(string='Use batch payments',
-        help='This allows you grouping payments into a single batch and eases the reconciliation process.\n'
-             '-This installs the account_batch_payment module.')
-    module_account_sepa = fields.Boolean(string='SEPA Credit Transfer (SCT)')
-    module_account_sepa_direct_debit = fields.Boolean(string='Use SEPA Direct Debit')
-    module_account_plaid = fields.Boolean(string="Plaid Connector")
-    module_account_yodlee = fields.Boolean("Bank Interface - Sync your bank feeds automatically")
-    module_account_bank_statement_import_qif = fields.Boolean("Import .qif files")
-    module_account_bank_statement_import_ofx = fields.Boolean("Import in .ofx format")
-    module_account_bank_statement_import_csv = fields.Boolean("Import in .csv format")
-    module_account_bank_statement_import_camt = fields.Boolean("Import in CAMT.053 format")
     module_currency_rate_live = fields.Boolean(string="Automatic Currency Rates")
-    module_account_intrastat = fields.Boolean(string='Intrastat')
     module_product_margin = fields.Boolean(string="Allow Product Margin")
     module_l10n_eu_service = fields.Boolean(string="EU Digital Goods VAT")
-    module_account_taxcloud = fields.Boolean(string="Account TaxCloud")
-    module_account_invoice_extract = fields.Boolean(string="Bill Digitalization")
-    module_snailmail_account = fields.Boolean(string="Snailmail")
     tax_exigibility = fields.Boolean(string='Cash Basis', related='company_id.tax_exigibility', readonly=False)
     tax_cash_basis_journal_id = fields.Many2one('account.journal', related='company_id.tax_cash_basis_journal_id', string="Tax Cash Basis Journal", readonly=False)
     account_cash_basis_base_account_id = fields.Many2one(
